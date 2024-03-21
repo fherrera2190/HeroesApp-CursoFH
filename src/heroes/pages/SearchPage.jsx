ddf1e938af663aed5efabb1d21135b8000a8ee7a
unknown <herrera.fernando1209@gmail.com>
2024-03-20 22:20:55 -0300

    App Heroes done!

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -9,9 +9,11 @@ import { useMemo } from "react";
 export const SearchPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const { searchText, onInputChange } = useForm({ searchText: "" });
   const { q = "" } = queryString.parse(location.search);
+  const { searchText, onInputChange } = useForm({ searchText: q });
   const heroes = useMemo(() => getHeroesByName(q), [q]);
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
   const onSearchSubmit = (e) => {
     e.preventDefault();
     if (searchText.trim().length < 1) return;
@@ -44,8 +46,24 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">
+          {/* {
+              ( q === '' )
+                ? <div className="alert alert-primary">Search a hero</div>
+                : ( heroes.length === 0 ) 
+                  && <div className="alert alert-danger">No hero with <b>{ q }</b></div>
+            } */}
+
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? "" : "none" }}
+          >
+            Search a hero
+          </div>
+
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? "" : "none" }}
+          >
             No hero with <b>{q}</b>
           </div>
 
