da794edc70367f9265b508928418c32b4f56d1b4
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-03-24 21:47:45 -0300

    user logout

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/context/AuthContext";
 
 export const Navbar = () => {
   const navigate = useNavigate();
-
+  const { user, logout } = useContext(AuthContext);
   const onLogout = () => {
+    logout();
     navigate("/login", { replace: true });
     console.log("Saliste");
   };
@@ -55,7 +58,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">Fernando</span>
+          <span className="nav-item nav-link text-primary">{user?.name}</span>
           <button onClick={onLogout} className="nav-item nav-link btn">
             Logut
           </button>
