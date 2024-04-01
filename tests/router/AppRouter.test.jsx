d69522e9023960fd5a218ddb409e676f75bd67f0
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-04-01 00:45:19 -0300

    Pruebas NavBar

--- tests/router/AppRouter.test.jsx
+++ tests/router/AppRouter.test.jsx
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 import { AuthContext } from "../../src/auth/context";
 import { MemoryRouter } from "react-router-dom";
 import { AppRouter } from "../../src/router/AppRouter";
+
+
 describe("Pruebas en <AppRouter/>", () => {
   test("Debe de motrar el login si no esta autenticado", () => {
     const contextValue = {
@@ -10,11 +12,32 @@ describe("Pruebas en <AppRouter/>", () => {
     render(
       <MemoryRouter initialEntries={["/marvel"]}>
         <AuthContext.Provider value={contextValue}>
-
-          <AppRouter/>
+          <AppRouter />
         </AuthContext.Provider>
       </MemoryRouter>
     );
-    screen.debug()
+    console.log(screen.getAllByText("Login").length)
+    expect(screen.getAllByText("Login").length).toBe(1);
+    // screen.debug()
+  });
+
+  test("Debe de mostrar el componente de Marvel si esta autenticado", () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        id: "ABC123",
+        name: "Fernando Martin",
+      },
+    };
+    render(
+      <MemoryRouter initialEntries={["/login"]}>
+        <AuthContext.Provider value={contextValue}>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter>
+    );
+    expect(screen.getAllByText("Marvel").length).toBeGreaterThan(1);
+
+    //screen.debug();
   });
 });
